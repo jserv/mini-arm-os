008e2409135d5118a7b744f553875cba5803c11f
Jeff Liaw <rampant1018@gmail.com>
2015-01-12 13:49:49 +0800

    Remove unused function prototype

--- 01-HelloWorld/startup.c
+++ 01-HelloWorld/startup.c
@@ -1,15 +1,10 @@
 #include <stdint.h>
-#include "reg.h"
 
 /* c code entry point */
 extern void c_entry(void);
 
 /* end address for the stack. defined in linker script */
 extern uint32_t _estack;
-
-void copy_data_init(void);
-void fill_bss_zero(void);
-void rcc_clock_init(void);
 
 void main(void)
 {
@@ -21,6 +16,6 @@ void main(void)
 
 __attribute ((section(".isr_vector")))
 uint32_t *isr_vectors[] = {
-	(uint32_t *)	&_estack,		// stack pointer
+	(uint32_t *)	_estack,		// stack pointer
 	(uint32_t *)	main,			// code entry point
 };
