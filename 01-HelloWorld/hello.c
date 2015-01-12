7afc97d79e960c9d5a543490cacbd06b9517f132
Jeff Liaw <rampant1018@gmail.com>
2015-01-12 10:51:08 +0800

    Use register map directly instead of calling API

--- 01-HelloWorld/hello.c
+++ 01-HelloWorld/hello.c
@@ -1,59 +1,37 @@
-#include <stm32f10x_usart.h>
-#include <stm32f10x_rcc.h>
-#include <stm32f10x_gpio.h>
+#include <stdint.h>
+#include "reg.h"
 
-void usart_init(void)
-{
-	USART_InitTypeDef USART2_InitStructure;
-	GPIO_InitTypeDef GPIO_InitStructure;
-
-	// ENable clocks BEFORE using/configuring peripherals that expect them to be running
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-
-	// Configure USART2 Tx (PA.2) as alternate function push-pull
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-	// Configure USART2 Rx (PA.3) as input floating
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-	// enable UART peripheral by activating clock
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
-
-	// USART2 configuration
-	USART2_InitStructure.USART_BaudRate = 115200;
-	USART2_InitStructure.USART_WordLength = USART_WordLength_8b;  //Word Length = 8 Bits
-	USART2_InitStructure.USART_StopBits = USART_StopBits_1;  //Two Stop Bit
-	USART2_InitStructure.USART_Parity = USART_Parity_No ;   //No parity
-	USART2_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;  //Hardware flow control disabled (RTS and CTS signals)
-	USART2_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;  //Receive and transmit enabled
-
-	//Configure USARTs
-	USART_Init(USART2,&USART2_InitStructure);
-
-	//Enable USARTs
-	USART_Cmd(USART2, ENABLE);
-}
+/* USART TXE Flag
+ * This flag is cleared when data is written to USARTx_DR and
+ * set when that data is transferred to the TDR
+ */
+#define USART_FLAG_TXE ((uint16_t)0x0080)
 
 void print_str(char *str)
 {
 	while(*str) {
-		while(!(USART2->SR & USART_FLAG_TXE));
-		USART2->DR = (*str & 0xFF);
+		while(!(*(USART2_SR) & USART_FLAG_TXE));
+		*(USART2_DR) = (*str & 0xFF);
 		str++;
 	}
 }
 
 void main() {
-	usart_init();
+	*(RCC_APB2ENR) |= (uint32_t)(0x00000001 | 0x00000004);
+	*(RCC_APB1ENR) |= (uint32_t)(0x00020000);
+
+	/* USART2 Configuration, Rx->PA3, Tx->PA2 */
+	*(GPIOA_CRL) = 0x00004B00;
+	*(GPIOA_CRH) = 0x44444444;
+	*(GPIOA_ODR) = 0x00000000;
+	*(GPIOA_BSRR) = 0x00000000;
+	*(GPIOA_BRR) = 0x00000000;
+
+	*(USART2_CR1) = 0x0000000C;
+	*(USART2_CR2) = 0x00000000;
+	*(USART2_CR3) = 0x00000000;
+	*(USART2_CR1) |= 0x2000;
+	
 	print_str("Hello World!\n");
 
 	while(1);
