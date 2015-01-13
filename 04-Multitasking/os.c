f8f48e85ab3245b6ac51b88777daca0d0c6b2a90
Jeff Liaw <rampant1018@gmail.com>
2015-01-13 15:37:06 +0800

    Port 04-Multitasking to stm32p103

--- 04-Multitasking/os.c
+++ 04-Multitasking/os.c
@@ -1,6 +1,7 @@
-#include "UART.h"
-#include "asm.h"
+#include <stdint.h>
 #include <stddef.h>
+#include "reg.h"
+#include "asm.h"
 
 /* Size of our user task stacks in words */
 #define STACK_SIZE 256
@@ -8,30 +9,53 @@
 /* Number of user task */
 #define TASK_LIMIT 2
 
-void write_serial(const char *s)
+/* USART TXE Flag
+ * This flag is cleared when data is written to USARTx_DR and
+ * set when that data is transferred to the TDR
+ */
+#define USART_FLAG_TXE ((uint16_t)0x0080)
+
+void usart_init(void)
 {
-	while (*s) {
-		while (*(UART0 + UARTFR) & UARTFR_TXFF);
-		*UART0 = *s;
-		s++;
+	*(RCC_APB2ENR) |= (uint32_t)(0x00000001 | 0x00000004);
+	*(RCC_APB1ENR) |= (uint32_t)(0x00020000);
+
+	/* USART2 Configuration, Rx->PA3, Tx->PA2 */
+	*(GPIOA_CRL) = 0x00004B00;
+	*(GPIOA_CRH) = 0x44444444;
+	*(GPIOA_ODR) = 0x00000000;
+	*(GPIOA_BSRR) = 0x00000000;
+	*(GPIOA_BRR) = 0x00000000;
+
+	*(USART2_CR1) = 0x0000000C;
+	*(USART2_CR2) = 0x00000000;
+	*(USART2_CR3) = 0x00000000;
+	*(USART2_CR1) |= 0x2000;
+}
+
+void print_str(char *str)
+{
+	while(*str) {
+		while(!(*(USART2_SR) & USART_FLAG_TXE));
+		*(USART2_DR) = (*str & 0xFF);
+		str++;
 	}
 }
 
-/* creates user task */
+/* Initilize user task stack */
 unsigned int *init_task(unsigned int *stack, void (*start)(void))
 {
 	stack += STACK_SIZE - 16; /* End of stack, minus what we are about to push */
-	stack[0] = 0x10; /* User mode, interrupts on */
-	stack[1] = (unsigned int) start;
+	stack[8] = (unsigned int) start;
 	return stack;
 }
 
 void task1_func(void)
 {
-	write_serial("task1: Executed!\n");
-	write_serial("task1: Now, return to kernel mode\n");
+	print_str("task1: Executed!\n");
+	print_str("task1: Now, return to kernel mode\n");
 	syscall();
-	write_serial("task1: About to enter kernel\n");
+	print_str("task1: About to enter kernel\n");
 	while (1)
 		syscall(); /* return to kernel mode */
 }
@@ -39,8 +63,8 @@ void task1_func(void)
 void task2_func(void)
 {
 	while (1) {
-		write_serial("task2: Executed!\n");
-		write_serial("task2: Now, return to kernel mode\n");
+		print_str("task2: Executed!\n");
+		print_str("task2: Now, return to kernel mode\n");
 		syscall(); /* return to kernel mode */
 	}
 }
@@ -57,8 +81,10 @@ int c_entry(void)
 	usertasks[1] = init_task(user_stacks[1], &task2_func);
 	task_count += 1;
 
-	write_serial("OS: Starting...\n");
-	write_serial("OS: Scheduler implementation : round-robin\n");
+	usart_init();
+
+	print_str("OS: Starting...\n");
+	print_str("OS: Scheduler implementation : round-robin\n");
 	while (1) {
 		usertasks[current_task] = activate(usertasks[current_task]);
 		current_task++;
