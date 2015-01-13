33c078ddf79b58148499ce7ff3496ff7881881c2
Jeff Liaw <rampant1018@gmail.com>
2015-01-13 16:27:37 +0800

    Port 05-TimerInterrupt to stm32p103

--- 05-TimerInterrupt/hello.c
+++ 05-TimerInterrupt/hello.c
@@ -1,81 +1,54 @@
-#include "TIMER.h"
-#include "VIC.h"
-#include "UART.h"
+#include <stdint.h>
+#include "reg.h"
 
-void print_uart0(const char *s)
+/* USART TXE Flag
+ * This flag is cleared when data is written to USARTx_DR and
+ * set when that data is transferred to the TDR
+ */
+#define USART_FLAG_TXE ((uint16_t)0x0080)
+
+void usart_init(void)
 {
-	while (*s != '\0') {
-		while (*(UART0 + UARTFR) & UARTFR_TXFF);
-		*UART0 = (unsigned int)(*s);
-		s++;
+	*(RCC_APB2ENR) |= (uint32_t)(0x00000001 | 0x00000004);
+	*(RCC_APB1ENR) |= (uint32_t)(0x00020000);
+
+	/* USART2 Configuration, Rx->PA3, Tx->PA2 */
+	*(GPIOA_CRL) = 0x00004B00;
+	*(GPIOA_CRH) = 0x44444444;
+	*(GPIOA_ODR) = 0x00000000;
+	*(GPIOA_BSRR) = 0x00000000;
+	*(GPIOA_BRR) = 0x00000000;
+
+	*(USART2_CR1) = 0x0000000C;
+	*(USART2_CR2) = 0x00000000;
+	*(USART2_CR3) = 0x00000000;
+	*(USART2_CR1) |= 0x2000;
+}
+
+void print_str(char *str)
+{
+	while(*str) {
+		while(!(*(USART2_SR) & USART_FLAG_TXE));
+		*(USART2_DR) = (*str & 0xFF);
+		str++;
 	}
 }
 
-void c_entry()
+void c_entry(void)
 {
-	int i = 0;
-	print_uart0("Hello world!\n");
+	usart_init();
 
-	/* VIC Configurations */
-	*VIC_INT_SELECT = 0;		/* All interrupts are IRQ */
-	*VIC_ENABLE_INT = 0x00000010;	/* Enable Timer01 Interrupt */
+	print_str("Hello world!\n");
 
-	/* Timer1 Configurations */
-	TIMER01_disable();
-	TIMER01_LOAD_VALUE = 65520;
-	TIMER01_enable();
+	/* SysTick configuration */
+	*SYSTICK_LOAD = 7200000;
+	*SYSTICK_VAL = 0;
+	*SYSTICK_CTRL = 0x07;
 
-	while (1)
-		/* wait */ ;
+	while(1); /* wait */
 }
 
-void __attribute__((interrupt)) irq_handler()
+void __attribute__((interrupt)) systick_handler(void)
 {
-	/* Determine the interrupt's source */
-	print_uart0("Interrupt raised\r\n");
-	switch (*VIC_BASE_ADDR) {
-	case 0x00000010:
-		print_uart0("Interrupt from Timer 1\r\n");
-		break;
-	default:
-		print_uart0("Interrupt not form Timer 1\r\n");
-	}
-}
-
-/* all other handlers are infinite loops (for the moment) */
-void __attribute__((interrupt)) undef_handler(void)
-{
-	for (;;);
-}
-
-void __attribute__((interrupt)) swi_handler(void)
-{
-	for (;;);
-}
-
-void __attribute__((interrupt)) prefetch_abort_handler(void)
-{
-	for (;;);
-}
-
-void __attribute__((interrupt)) data_abort_handler(void)
-{
-	for (;;);
-}
-
-void __attribute__((interrupt)) fiq_handler(void)
-{
-	for (;;);
-}
-
-/* Copy the vector table to the 0x00000000 adddress */
-void copy_vectors(void)
-{
-	extern unsigned int vectors_start;
-	extern unsigned int vectors_end;
-	unsigned int *vectors_src = &vectors_start;
-	unsigned int *vectors_dst = (unsigned int *) 0;
-
-	while (vectors_src < &vectors_end)
-		*vectors_dst++ = *vectors_src++;
+	print_str("Interrupt from System Timer\n");
 }
