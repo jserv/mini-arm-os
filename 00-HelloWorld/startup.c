0f913bf650443106deaef022d990274d68e2bcc8
Jim Huang <jserv@0xlab.org>
2015-02-11 17:31:03 +0800

    00-HelloWorld: XIP only, no data section is used

--- 00-HelloWorld/startup.c
+++ 00-HelloWorld/startup.c
@@ -1,21 +1,14 @@
 #include <stdint.h>
 
-/* main program entry point */
 extern void main(void);
-
-/* end address for the stack. defined in linker script */
-extern uint32_t _estack;
-
 void reset_handler(void)
 {
-	/* System Initialization Here */
-
-	/* C Code Entry Point */
+	/* jump to C entry point */
 	main();
 }
 
 __attribute((section(".isr_vector")))
 uint32_t *isr_vectors[] = {
-	(uint32_t *) &_estack,		/* stack pointer */
+	0,
 	(uint32_t *) reset_handler,	/* code entry point */
 };
