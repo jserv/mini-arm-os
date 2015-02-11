86601d6df3c7f7875b1e6e557aa71201fe9587ec
Jim Huang <jserv@0xlab.org>
2015-02-11 14:38:48 +0800

    Remove unused ARM PrimeCell PL011 UART0

--- 03-ContextSwitch-2/UART.h
+++
@@ -1,8 +0,0 @@
-/* ARM PrimeCell PL011 UART0 */
-#define UART0		((volatile unsigned int *) 0x101F1000)
-
-/* UARTFR (Flag register) */
-#define UARTFR		0x06
-
-/* bit 3  : Busy - bit 5 : Transmission Fifo Full */
-#define UARTFR_TXFF	0x20
