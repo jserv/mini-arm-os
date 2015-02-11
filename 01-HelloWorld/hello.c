e440d63884446977dc4b62d09788a0be476db7ba
Jim Huang <jserv@0xlab.org>
2015-02-11 17:07:54 +0800

    trivial syntax tweak

--- 01-HelloWorld/hello.c
+++ 01-HelloWorld/hello.c
@@ -18,8 +18,8 @@ void print_str(const char *str)
 
 void main(void)
 {
-	*(RCC_APB2ENR) |= (uint32_t)(0x00000001 | 0x00000004);
-	*(RCC_APB1ENR) |= (uint32_t)(0x00020000);
+	*(RCC_APB2ENR) |= (uint32_t) (0x00000001 | 0x00000004);
+	*(RCC_APB1ENR) |= (uint32_t) (0x00020000);
 
 	/* USART2 Configuration, Rx->PA3, Tx->PA2 */
 	*(GPIOA_CRL) = 0x00004B00;
