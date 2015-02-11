ca40d336ccd1a260798a70f369e5e5478e105528
Jim Huang <jserv@0xlab.org>
2015-02-11 15:07:57 +0800

    01-HelloWorld: syntax tweak

--- 01-HelloWorld/startup_full.c
+++ 01-HelloWorld/startup_full.c
@@ -9,18 +9,18 @@
 
 /* Bit definition for RCC_CFGR register */
 #define RCC_CFGR_SW		((uint32_t) 0x00000003)	/*!< SW[1:0] bits (System clock Switch) */
-#define RCC_CFGR_SW_HSE	((uint32_t) 0x00000001)	/*!< HSE selected as system clock */
+#define RCC_CFGR_SW_HSE	((uint32_t) 0x00000001)		/*!< HSE selected as system clock */
 #define RCC_CFGR_SWS		((uint32_t) 0x0000000C)	/*!< SWS[1:0] bits (System Clock Switch Status) */
 #define RCC_CFGR_HPRE_DIV1	((uint32_t) 0x00000000)	/*!< SYSCLK not divided */
 #define RCC_CFGR_PPRE1_DIV1	((uint32_t) 0x00000000)	/*!< HCLK not divided */
 #define RCC_CFGR_PPRE2_DIV1	((uint32_t) 0x00000000)	/*!< HCLK not divided */
 
 /* Bit definition for FLASH_ACR register */
-#define FLASH_ACR_LATENCY	((uint8_t) 0x03)		/*!< LATENCY[2:0] bits (Latency) */
-#define FLASH_ACR_LATENCY_0	((uint8_t) 0x00)		/*!< Bit 0 */
-#define FLASH_ACR_PRFTBE	((uint8_t) 0x10)		/*!< Prefetch Buffer Enable */
+#define FLASH_ACR_LATENCY	((uint8_t) 0x03)	/*!< LATENCY[2:0] bits (Latency) */
+#define FLASH_ACR_LATENCY_0	((uint8_t) 0x00)	/*!< Bit 0 */
+#define FLASH_ACR_PRFTBE	((uint8_t) 0x10)	/*!< Prefetch Buffer Enable */
 
-#define HSE_STARTUP_TIMEOUT	((uint16_t)0x0500)	/*!< Time out for HSE start up */
+#define HSE_STARTUP_TIMEOUT	((uint16_t) 0x0500)	/*!< Time out for HSE start up */
 
 /* main program entry point */
 extern void main(void);
@@ -62,29 +62,29 @@ void reset_handler(void)
 
 __attribute((section(".isr_vector")))
 uint32_t *isr_vectors[] = {
-	(uint32_t *)	&_estack,		// stack pointer
-	(uint32_t *)	reset_handler,		// code entry point
-	(uint32_t *)	nmi_handler,		// NMI handler
-	(uint32_t *)	hardfault_handler	// hard fault handler
+	(uint32_t *) &_estack,		/* stack pointer */
+	(uint32_t *) reset_handler,	/* code entry point */
+	(uint32_t *) nmi_handler,	/* NMI handler */
+	(uint32_t *) hardfault_handler	/* hard fault handler */
 };
 
 void rcc_clock_init(void)
 {
 	/* Reset the RCC clock configuration to the default reset state(for debug purpose) */
 	/* Set HSION bit */
-	*RCC_CR |= (uint32_t)0x00000001;
+	*RCC_CR |= (uint32_t) 0x00000001;
 
 	/* Reset SW, HPRE, PPRE1, PPRE2, ADCPRE and MCO bits */
-	*RCC_CFGR &= (uint32_t)0xF8FF0000;
+	*RCC_CFGR &= (uint32_t) 0xF8FF0000;
 
 	/* Reset HSEON, CSSON and PLLON bits */
-	*RCC_CR &= (uint32_t)0xFEF6FFFF;
+	*RCC_CR &= (uint32_t) 0xFEF6FFFF;
 
 	/* Reset HSEBYP bit */
-	*RCC_CR &= (uint32_t)0xFFFBFFFF;
+	*RCC_CR &= (uint32_t) 0xFFFBFFFF;
 
 	/* Reset PLLSRC, PLLXTPRE, PLLMUL and USBPRE/OTGFSPRE bits */
-	*RCC_CFGR &= (uint32_t)0xFF80FFFF;
+	*RCC_CFGR &= (uint32_t) 0xFF80FFFF;
 
 	/* Disable all interrupts and clear pending bits  */
 	*RCC_CIR = 0x009F0000;
@@ -95,7 +95,7 @@ void rcc_clock_init(void)
 
 	/* SYSCLK, HCLK, PCLK2 and PCLK1 configuration ---------------------------*/
 	/* Enable HSE */
-	*RCC_CR |= ((uint32_t)RCC_CR_HSEON);
+	*RCC_CR |= (uint32_t) RCC_CR_HSEON;
 
 	/* Wait till HSE is ready and if Time out is reached exit */
 	do {
@@ -113,7 +113,7 @@ void rcc_clock_init(void)
 		*FLASH_ACR |= FLASH_ACR_PRFTBE;
 
 		/* Flash 0 wait state */
-		*FLASH_ACR &= (uint32_t)((uint32_t) ~FLASH_ACR_LATENCY);
+		*FLASH_ACR &= (uint32_t) ((uint32_t) ~FLASH_ACR_LATENCY);
 
 		*FLASH_ACR |= (uint32_t) FLASH_ACR_LATENCY_0;
 
@@ -127,8 +127,8 @@ void rcc_clock_init(void)
 		*RCC_CFGR |= (uint32_t) RCC_CFGR_PPRE1_DIV1;
 
 		/* Select HSE as system clock source */
-		*RCC_CFGR &= (uint32_t)((uint32_t) ~(RCC_CFGR_SW));
-		*RCC_CFGR |= (uint32_t)RCC_CFGR_SW_HSE;
+		*RCC_CFGR &= (uint32_t) ((uint32_t) ~(RCC_CFGR_SW));
+		*RCC_CFGR |= (uint32_t) RCC_CFGR_SW_HSE;
 
 		/* Wait till HSE is used as system clock source */
 		while ((*RCC_CFGR & (uint32_t) RCC_CFGR_SWS) != (uint32_t) 0x04);
