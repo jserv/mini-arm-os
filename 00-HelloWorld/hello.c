f1fba381b81fc71aa902645d5f1532ac63a44cad
Jim Huang <jserv@0xlab.org>
2015-02-11 16:47:47 +0800

    00-HelloWorld: implement puts

--- 00-HelloWorld/hello.c
+++ 00-HelloWorld/hello.c
@@ -7,19 +7,19 @@
  */
 #define USART_FLAG_TXE	((uint16_t) 0x0080)
 
-void print_str(const char *str)
+int puts(const char *str)
 {
 	while (*str) {
 		while (!(*(USART2_SR) & USART_FLAG_TXE));
-		*(USART2_DR) = (*str & 0xFF);
-		str++;
+		*(USART2_DR) = *str++ & 0xFF;
 	}
+	return 0;
 }
 
 void main(void)
 {
-	*(RCC_APB2ENR) |= (uint32_t)(0x00000001 | 0x00000004);
-	*(RCC_APB1ENR) |= (uint32_t)(0x00020000);
+	*(RCC_APB2ENR) |= (uint32_t) (0x00000001 | 0x00000004);
+	*(RCC_APB1ENR) |= (uint32_t) (0x00020000);
 
 	/* USART2 Configuration, Rx->PA3, Tx->PA2 */
 	*(GPIOA_CRL) = 0x00004B00;
@@ -33,7 +33,7 @@ void main(void)
 	*(USART2_CR3) = 0x00000000;
 	*(USART2_CR1) |= 0x2000;
 
-	print_str("Hello World!\n");
+	puts("Hello World!\n");
 
 	while (1);
 }
