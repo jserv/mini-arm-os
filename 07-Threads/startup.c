848e7bd972054dee90bd26a50d638b78b4429b9b
JaredCJR <empire89090@gmail.com>
2016-05-02 23:45:03 +0800

    Refactoring Makefile for more device-friendly
    The output files moves into ./release
    Using more variables in the Makefile.
    Add platform directory

--- 07-Threads/startup.c
+++ 07-Threads/startup.c
@@ -1,26 +1,6 @@
 #include <stdint.h>
 #include "reg.h"
 
-/* Bit definition for RCC_CR register */
-#define RCC_CR_HSION	((uint32_t) 0x00000001)		/*!< Internal High Speed clock enable */
-#define RCC_CR_HSEON	((uint32_t) 0x00010000)		/*!< External High Speed clock enable */
-#define RCC_CR_HSERDY	((uint32_t) 0x00020000)		/*!< External High Speed clock ready flag */
-#define RCC_CR_CSSON	((uint32_t) 0x00080000)		/*!< Clock Security System enable */
-
-/* Bit definition for RCC_CFGR register */
-#define  RCC_CFGR_SW		((uint32_t) 0x00000003)	/*!< SW[1:0] bits (System clock Switch) */
-#define  RCC_CFGR_SW_HSE	((uint32_t) 0x00000001)	/*!< HSE selected as system clock */
-#define  RCC_CFGR_SWS		((uint32_t) 0x0000000C)	/*!< SWS[1:0] bits (System Clock Switch Status) */
-#define  RCC_CFGR_HPRE_DIV1	((uint32_t) 0x00000000)	/*!< SYSCLK not divided */
-#define  RCC_CFGR_PPRE1_DIV1	((uint32_t) 0x00000000)	/*!< HCLK not divided */
-#define  RCC_CFGR_PPRE2_DIV1	((uint32_t) 0x00000000)	/*!< HCLK not divided */
-
-/* Bit definition for FLASH_ACR register */
-#define FLASH_ACR_LATENCY	((uint8_t) 0x03)	/*!< LATENCY[2:0] bits (Latency) */
-#define FLASH_ACR_LATENCY_0	((uint8_t) 0x00)	/*!< Bit 0 */
-#define FLASH_ACR_PRFTBE	((uint8_t) 0x10)	/*!< Prefetch Buffer Enable */
-
-#define HSE_STARTUP_TIMEOUT	((uint16_t) 0x0500)	/*!< Time out for HSE start up */
 
 /* main program entry point */
 extern void main(void);
