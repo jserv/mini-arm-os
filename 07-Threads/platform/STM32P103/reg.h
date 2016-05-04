8631dae1c62568fbf118f79597d446ee625f1fb1
JaredCJR <empire89090@gmail.com>
2016-05-04 19:43:12 +0800

    Move HW dependent code out of independent source files.
    Specifically,"usart" relevant code.

--- 07-Threads/platform/STM32P103/reg.h
+++ 07-Threads/platform/STM32P103/reg.h
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 #ifndef __REG_H_
 #define __REG_H_
 
---
+++ 07-Threads/platform/STM32P103/uart.c
@@ -0,0 +1,36 @@
+#include "uart.h"
+#include "reg.h"
+
+/* USART TXE Flag
+ * This flag is cleared when data is written to USARTx_DR and
+ * set when that data is transferred to the TDR
+ */
+#define USART_FLAG_TXE  ((uint16_t) 0x0080)
+
+void usart_init(void)                                                                                                                   
+{
+    *(RCC_APB2ENR) |= (uint32_t) (0x00000001 | 0x00000004);
+    *(RCC_APB1ENR) |= (uint32_t) (0x00020000);
+
+    /* USART2 Configuration, Rx->PA3, Tx->PA2 */
+    *(GPIOA_CRL) = 0x00004B00;
+    *(GPIOA_CRH) = 0x44444444;
+    *(GPIOA_ODR) = 0x00000000;
+    *(GPIOA_BSRR) = 0x00000000;
+    *(GPIOA_BRR) = 0x00000000;
+
+    *(USART2_CR1) = 0x0000000C;
+    *(USART2_CR2) = 0x00000000;
+    *(USART2_CR3) = 0x00000000;
+    *(USART2_CR1) |= 0x2000;
+}
+
+void print_str(const char *str)
+{
+    while (*str) {
+        while (!(*(USART2_SR) & USART_FLAG_TXE));
+        *(USART2_DR) = (*str & 0xFF);
+        str++;
+    }
+}
+
