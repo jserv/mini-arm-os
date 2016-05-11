437a3d9805760620f73a5d731f01bfc0fe6940f9
JaredCJR <empire89090@gmail.com>
2016-05-11 15:04:31 +0800

    Update cmsis version and reg.h "include" file in f429disco.

--- 08-CMSIS/platform/f429disco/include/reg.h
+++ 08-CMSIS/platform/f429disco/include/reg.h
@@ -1,8 +1,7 @@
 #ifndef __REG_H_
 #define __REG_H_
 
-#include "stm32f4xx.h"
-#include "core_cm4.h"
+#include "cmsis.h"
 
 #define HSE_STARTUP_TIMEOUT ((uint16_t) 0x0500) /*!< Time out for HSE start up */
 #define RECOMMAND_TIME_INTERVAL 5
