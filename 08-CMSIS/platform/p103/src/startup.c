d02705f863c3cc681f070cf07b1082698fc8765a
JaredCJR <empire89090@gmail.com>
2016-05-11 15:20:39 +0800

    Let p103 using cmsis submodule,also update the version of "cmsis".

--- 08-CMSIS/platform/p103/src/startup.c
+++ 08-CMSIS/platform/p103/src/startup.c
@@ -72,22 +72,22 @@ void rcc_clock_init(void)
 {
 	/* Reset the RCC clock configuration to the default reset state(for debug purpose) */
 	/* Set HSION bit */
-	*RCC_CR |= (uint32_t) 0x00000001;
+	RCC->CR |= (uint32_t) 0x00000001;
 
 	/* Reset SW, HPRE, PPRE1, PPRE2, ADCPRE and MCO bits */
-	*RCC_CFGR &= (uint32_t) 0xF8FF0000;
+	RCC->CFGR &= (uint32_t) 0xF8FF0000;
 
 	/* Reset HSEON, CSSON and PLLON bits */
-	*RCC_CR &= (uint32_t) 0xFEF6FFFF;
+	RCC->CR &= (uint32_t) 0xFEF6FFFF;
 
 	/* Reset HSEBYP bit */
-	*RCC_CR &= (uint32_t) 0xFFFBFFFF;
+	RCC->CR &= (uint32_t) 0xFFFBFFFF;
 
 	/* Reset PLLSRC, PLLXTPRE, PLLMUL and USBPRE/OTGFSPRE bits */
-	*RCC_CFGR &= (uint32_t) 0xFF80FFFF;
+	RCC->CFGR &= (uint32_t) 0xFF80FFFF;
 
 	/* Disable all interrupts and clear pending bits  */
-	*RCC_CIR = 0x009F0000;
+	RCC->CIR = 0x009F0000;
 
 	/* Configure the System clock frequency, HCLK, PCLK2 and PCLK1 prescalers */
 	/* Configure the Flash Latency cycles and enable prefetch buffer */
@@ -95,43 +95,43 @@ void rcc_clock_init(void)
 
 	/* SYSCLK, HCLK, PCLK2 and PCLK1 configuration ---------------------------*/
 	/* Enable HSE */
-	*RCC_CR |= ((uint32_t)RCC_CR_HSEON);
+	RCC->CR |= ((uint32_t)RCC_CR_HSEON);
 
 	/* Wait till HSE is ready and if Time out is reached exit */
 	do {
-		HSEStatus = *RCC_CR & RCC_CR_HSERDY;
+		HSEStatus = RCC->CR & RCC_CR_HSERDY;
 		StartUpCounter++;
 	} while ((HSEStatus == 0) && (StartUpCounter != HSE_STARTUP_TIMEOUT));
 
-	if ((*RCC_CR & RCC_CR_HSERDY) != 0)
+	if ((RCC->CR & RCC_CR_HSERDY) != 0)
 		HSEStatus = (uint32_t) 0x01;
 	else
 		HSEStatus = (uint32_t) 0x00;
 
 	if (HSEStatus == (uint32_t) 0x01) {
 		/* Enable Prefetch Buffer */
-		*FLASH_ACR |= FLASH_ACR_PRFTBE;
+		FLASH->ACR |= FLASH_ACR_PRFTBE;
 
 		/* Flash 0 wait state */
-		*FLASH_ACR &= (uint32_t)((uint32_t) ~FLASH_ACR_LATENCY);
+		FLASH->ACR &= (uint32_t)((uint32_t) ~FLASH_ACR_LATENCY);
 
-		*FLASH_ACR |= (uint32_t) FLASH_ACR_LATENCY_0;
+		FLASH->ACR |= (uint32_t) FLASH_ACR_LATENCY_0;
 
 		/* HCLK = SYSCLK */
-		*RCC_CFGR |= (uint32_t) RCC_CFGR_HPRE_DIV1;
+		RCC->CFGR |= (uint32_t) RCC_CFGR_HPRE_DIV1;
 
 		/* PCLK2 = HCLK */
-		*RCC_CFGR |= (uint32_t) RCC_CFGR_PPRE2_DIV1;
+		RCC->CFGR |= (uint32_t) RCC_CFGR_PPRE2_DIV1;
 
 		/* PCLK1 = HCLK */
-		*RCC_CFGR |= (uint32_t) RCC_CFGR_PPRE1_DIV1;
+		RCC->CFGR |= (uint32_t) RCC_CFGR_PPRE1_DIV1;
 
 		/* Select HSE as system clock source */
-		*RCC_CFGR &= (uint32_t)((uint32_t) ~(RCC_CFGR_SW));
-		*RCC_CFGR |= (uint32_t) RCC_CFGR_SW_HSE;
+		RCC->CFGR &= (uint32_t)((uint32_t) ~(RCC_CFGR_SW));
+		RCC->CFGR |= (uint32_t) RCC_CFGR_SW_HSE;
 
 		/* Wait till HSE is used as system clock source */
-		while ((*RCC_CFGR & (uint32_t) RCC_CFGR_SWS) != (uint32_t) 0x04);
+		while ((RCC->CFGR & (uint32_t) RCC_CFGR_SWS) != (uint32_t) 0x04);
 	} else {
 		/* If HSE fails to start-up, the application will have wrong clock
 		configuration. User can add here some code to deal with this error */
--- 08-CMSIS/platform/p103/src/systick.c
+++ 08-CMSIS/platform/p103/src/systick.c
@@ -1,17 +1,15 @@
 #include "reg.h"
 #include "systick.h"
 
-
 void systick_handler()
 {
-	*SCB_ICSR |= SCB_ICSR_PENDSVSET;
+	SCB->ICSR |= SCB_ICSR_PENDSVSET;
 }
-
 
 void SysTick_init(void)
 {
 	/* SysTick configuration */
-	*SYSTICK_LOAD = (CPU_CLOCK_HZ / TICK_RATE_HZ) - 1UL;
-	*SYSTICK_VAL = 0;
-	*SYSTICK_CTRL = 0x07;
+	SysTick->LOAD = (CPU_CLOCK_HZ / TICK_RATE_HZ) - 1UL;
+	SysTick->VAL = 0;
+	SysTick->CTRL = 0x07;
 }
