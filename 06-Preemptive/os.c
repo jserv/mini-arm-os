db1bb7bd0e0e23d58fa4deed972b109c01a38bd1
ryanpatiency <ryanpatiency@gmail.com>
2018-05-09 13:52:16 +0800

    Remove init_activate_env and task_init
    These two functions are no longer needed now and shall be deleted

--- 06-Preemptive/os.c
+++ 06-Preemptive/os.c
@@ -4,16 +4,16 @@
 #include "asm.h"
 
 /* Size of our user task stacks in words */
-#define STACK_SIZE	256
+#define STACK_SIZE 256
 
 /* Number of user task */
-#define TASK_LIMIT	3
+#define TASK_LIMIT 3
 
 /* USART TXE Flag
  * This flag is cleared when data is written to USARTx_DR and
  * set when that data is transferred to the TDR
  */
-#define USART_FLAG_TXE	((uint16_t) 0x0080)
+#define USART_FLAG_TXE ((uint16_t) 0x0080)
 
 /* 72MHz */
 #define CPU_CLOCK_HZ 72000000
@@ -23,41 +23,43 @@
 
 void usart_init(void)
 {
-	*(RCC_APB2ENR) |= (uint32_t) (0x00000001 | 0x00000004);
-	*(RCC_APB1ENR) |= (uint32_t) (0x00020000);
+    *(RCC_APB2ENR) |= (uint32_t)(0x00000001 | 0x00000004);
+    *(RCC_APB1ENR) |= (uint32_t)(0x00020000);
 
-	/* USART2 Configuration, Rx->PA3, Tx->PA2 */
-	*(GPIOA_CRL) = 0x00004B00;
-	*(GPIOA_CRH) = 0x44444444;
-	*(GPIOA_ODR) = 0x00000000;
-	*(GPIOA_BSRR) = 0x00000000;
-	*(GPIOA_BRR) = 0x00000000;
+    /* USART2 Configuration, Rx->PA3, Tx->PA2 */
+    *(GPIOA_CRL) = 0x00004B00;
+    *(GPIOA_CRH) = 0x44444444;
+    *(GPIOA_ODR) = 0x00000000;
+    *(GPIOA_BSRR) = 0x00000000;
+    *(GPIOA_BRR) = 0x00000000;
 
-	*(USART2_CR1) = 0x0000000C;
-	*(USART2_CR2) = 0x00000000;
-	*(USART2_CR3) = 0x00000000;
-	*(USART2_CR1) |= 0x2000;
+    *(USART2_CR1) = 0x0000000C;
+    *(USART2_CR2) = 0x00000000;
+    *(USART2_CR3) = 0x00000000;
+    *(USART2_CR1) |= 0x2000;
 }
 
 void print_str(const char *str)
 {
-	while (*str) {
-		while (!(*(USART2_SR) & USART_FLAG_TXE));
-		*(USART2_DR) = (*str & 0xFF);
-		str++;
-	}
+    while (*str) {
+        while (!(*(USART2_SR) &USART_FLAG_TXE))
+            ;
+        *(USART2_DR) = (*str & 0xFF);
+        str++;
+    }
 }
 
 void delay(volatile int count)
 {
-	count *= 50000;
-	while (count--);
+    count *= 50000;
+    while (count--)
+        ;
 }
 
 /* Exception return behavior */
-#define HANDLER_MSP	0xFFFFFFF1
-#define THREAD_MSP	0xFFFFFFF9
-#define THREAD_PSP	0xFFFFFFFD
+#define HANDLER_MSP 0xFFFFFFF1
+#define THREAD_MSP 0xFFFFFFF9
+#define THREAD_PSP 0xFFFFFFFD
 
 /* Initialize user task stack and execute it one time.
  * We set `THREAD_PSP` to `lr` to ensure that exception
@@ -66,77 +68,70 @@ void delay(volatile int count)
  */
 unsigned int *create_task(unsigned int *stack, void (*start)(void))
 {
-	stack += STACK_SIZE - 17; /* End of stack, minus what we are about to push */
-	stack[8] = (unsigned int) THREAD_PSP;
-	stack[15] = (unsigned int) start;
-	stack[16] = (unsigned int) 0x01000000; /* PSR Thumb bit */
-	stack = activate(stack);
+    stack +=
+        STACK_SIZE - 17; /* End of stack, minus what we are about to push */
+    stack[8] = (unsigned int) THREAD_PSP;
+    stack[15] = (unsigned int) start;
+    stack[16] = (unsigned int) 0x01000000; /* PSR Thumb bit */
+    stack = activate(stack);
 
-	return stack;
-}
-
-void task_init(void)
-{
-	unsigned int null_stacks[32];
-	init_activate_env(&null_stacks[32]);
+    return stack;
 }
 
 void task1_func(void)
 {
-	print_str("task1: Created!\n");
-	print_str("task1: Now, return to kernel mode\n");
-	syscall();
-	while (1) {
-		print_str("task1: Running...\n");
-		delay(1000);
-	}
+    print_str("task1: Created!\n");
+    print_str("task1: Now, return to kernel mode\n");
+    syscall();
+    while (1) {
+        print_str("task1: Running...\n");
+        delay(1000);
+    }
 }
 
 void task2_func(void)
 {
-	print_str("task2: Created!\n");
-	print_str("task2: Now, return to kernel mode\n");
-	syscall();
-	while (1) {
-		print_str("task2: Running...\n");
-		delay(1000);
-	}
+    print_str("task2: Created!\n");
+    print_str("task2: Now, return to kernel mode\n");
+    syscall();
+    while (1) {
+        print_str("task2: Running...\n");
+        delay(1000);
+    }
 }
 
 int main(void)
 {
-	unsigned int user_stacks[TASK_LIMIT][STACK_SIZE];
-	unsigned int *usertasks[TASK_LIMIT];
-	size_t task_count = 0;
-	size_t current_task;
+    unsigned int user_stacks[TASK_LIMIT][STACK_SIZE];
+    unsigned int *usertasks[TASK_LIMIT];
+    size_t task_count = 0;
+    size_t current_task;
 
-	usart_init();
+    usart_init();
 
-	task_init();
+    print_str("OS: Starting...\n");
+    print_str("OS: First create task 1\n");
+    usertasks[0] = create_task(user_stacks[0], &task1_func);
+    task_count += 1;
+    print_str("OS: Back to OS, create task 2\n");
+    usertasks[1] = create_task(user_stacks[1], &task2_func);
+    task_count += 1;
 
-	print_str("OS: Starting...\n");
-	print_str("OS: First create task 1\n");
-	usertasks[0] = create_task(user_stacks[0], &task1_func);
-	task_count += 1;
-	print_str("OS: Back to OS, create task 2\n");
-	usertasks[1] = create_task(user_stacks[1], &task2_func);
-	task_count += 1;
+    print_str("\nOS: Start round-robin scheduler!\n");
 
-	print_str("\nOS: Start round-robin scheduler!\n");
+    /* SysTick configuration */
+    *SYSTICK_LOAD = (CPU_CLOCK_HZ / TICK_RATE_HZ) - 1UL;
+    *SYSTICK_VAL = 0;
+    *SYSTICK_CTRL = 0x07;
+    current_task = 0;
 
-	/* SysTick configuration */
-	*SYSTICK_LOAD = (CPU_CLOCK_HZ / TICK_RATE_HZ) - 1UL;
-	*SYSTICK_VAL = 0;
-	*SYSTICK_CTRL = 0x07;
-	current_task = 0;
+    while (1) {
+        print_str("OS: Activate next task\n");
+        usertasks[current_task] = activate(usertasks[current_task]);
+        print_str("OS: Back to OS\n");
 
-	while (1) {
-		print_str("OS: Activate next task\n");
-		usertasks[current_task] = activate(usertasks[current_task]);
-		print_str("OS: Back to OS\n");
+        current_task = current_task == (task_count - 1) ? 0 : current_task + 1;
+    }
 
-		current_task = current_task == (task_count - 1) ? 0 : current_task + 1;
-	}
-
-	return 0;
+    return 0;
 }
